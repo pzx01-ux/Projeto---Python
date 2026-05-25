5616301a12b274ee8453cea2abf0805eb840ab3f
pablo <ph591479@gmail.com>
2026-05-25 15:13:08 -0300

    jogo de adivinhação finalizado

--- jogo.adivinhação.py
+++ jogo.adivinhação.py
@@ -1,1 +1,31 @@
-print('teste')
+from  random import randint
+
+resposta = randint(1, 100)
+
+Tentativas = 0
+
+print('Bem-vindo ao jogo de adivinhação!')
+
+palpite = int(input('Digite um número entre 1 e 100: '))
+
+print("O computador escolheu o número 1 a 100 você consegue adivinhar qual é?")
+print(f"Digite um número entre 1 e 100: {palpite}")
+
+while palpite != resposta:
+    Tentativas += 1
+
+    if palpite < resposta:
+        print('O número é maior do que o seu palpite.')
+    elif palpite > resposta:
+        print('O número é menor do que o seu palpite.')
+
+    palpite = int(input('Digite outro número: '))
+    
+print(f'Parabéns! Você acertou o número! o computador escolheu {resposta} e você digitou {palpite}.')
+
+if Tentativas <= 5:
+    print(f'Você fez {Tentativas} tentativas execelentes!')
+elif Tentativas >= 6 and Tentativas <= 10:
+    print(f'Você fez {Tentativas} tentativas muito bom!')
+elif Tentativas >= 10:
+    print(f'Você fez {Tentativas} tentativas, tente melhorar na próxima vez!') 
