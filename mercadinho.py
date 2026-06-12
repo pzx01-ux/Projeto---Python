4e1229496f28649b6f9b38f18a3f59da3d3ed809
pablo <ph591479@gmail.com>
2026-06-12 10:52:44 -0300

    cadastro de produto pronto

---
+++ mercadinho.py
@@ -0,0 +1,41 @@
+import json
+produto = 0
+valor = 0
+with open ("estoque.json", "r") as f:
+    try:
+        lista_produto = json.laod(f)
+    except:
+        lista_produto = []
+
+
+def cadastra_produto():
+    produto = str(input("Digite o nome do produto: "))
+    valor = float(input("Digite o valor do produto: "))
+    q_estoque = int(input("Digite o quantidade em estoque: "))
+    produto2 = {
+        "produto": produto , 
+        "preco": valor ,
+        "quantidade":  q_estoque
+    }
+    lista_produto.append(produto2)
+    with open("estoque.json" , "a") as f:
+        json.dump(produto2 , f, indent=4)
+    return lista_produto
+
+def ver_lista(lista_produto):
+    for produto2 in lista_produto:
+        print(f"Produto: {produto2["produto"]} | Preço: R${produto2["preco"]} | Estoque {produto2["quantidade"]}")
+
+while True:
+    print("1- Cadastra Produto")
+    print("2- Listar Produto")
+    print("3- Realizar Venda")
+    print("4- Ver Relatório do Dia")
+    print("5- Sair")
+
+    opcao = int(input("Digite a Opção Desejada: "))
+
+    if opcao == 1:
+       lista_produto = cadastra_produto ()
+    elif opcao == 2:
+        ver_lista(lista_produto)
